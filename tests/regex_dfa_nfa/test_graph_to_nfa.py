a6fa6fba0b52253ad906d7eba448e9874b0a194e
AlexanderZadorozhnyy <alexander.zadorozhnyy@yandex.ru>
2023-09-20 18:17:11 +0300

    Added more tests

--- tests/regex_dfa_nfa/test_graph_to_nfa.py
+++ tests/regex_dfa_nfa/test_graph_to_nfa.py
@@ -1,5 +1,7 @@
 from networkx import MultiDiGraph
 from pyformlang.finite_automaton import NondeterministicFiniteAutomaton
+
+from project.cfpq.utils import create_labeled_two_cycles_graph
 from project.graph.graph_worker import GraphWorker
 
 
@@ -61,6 +63,19 @@ def test_convert_graph_to_nfa_with_graph_final_nodes():
     assert expected == gw.convert_to_nfa()
 
 
+def test_convert_graph_to_nfa_with_graph_start_final_nodes():
+    expected = NondeterministicFiniteAutomaton()
+    expected.add_final_state(3)
+    expected.add_start_state(1)
+
+    graph = MultiDiGraph([(1, 0), (1, 2), (2, 3)])
+    graph.add_node(3, is_final=True)
+    graph.add_node(1, is_start=True)
+    gw = GraphWorker(graph)
+
+    assert expected == gw.convert_to_nfa()
+
+
 def test_convert_graph_to_nfa_with_start_nodes():
     expected = NondeterministicFiniteAutomaton()
     expected.add_start_state(0)
@@ -79,5 +94,40 @@ def test_convert_graph_to_nfa_with_final_nodes():
         expected.add_start_state(s)
 
     gw = GraphWorker(MultiDiGraph([(0, 1), (1, 2), (2, 3)]))
+
+    assert expected == gw.convert_to_nfa(final={0})
+
+
+def test_convert_graph_to_nfa_with_final_start_nodes():
+    expected = NondeterministicFiniteAutomaton()
+    expected.add_final_state(0)
+    expected.add_start_state(1)
+
+    gw = GraphWorker(MultiDiGraph([(1, 0), (1, 2)]))
+
+    assert expected == gw.convert_to_nfa(final={0}, start={1})
+
+
+def test_convert_two_cycled_graph_to_nfa():
+    expected = NondeterministicFiniteAutomaton()
+    expected.add_final_state(0)
+    for s in range(0, 5):
+        expected.add_start_state(s)
+
+    expected.add_transitions([(0, "1", 3)])
+    expected.add_transitions([(0, "0", 1)])
+    expected.add_transitions([(1, "0", 2)])
+    expected.add_transitions([(2, "0", 0)])
+    expected.add_transitions([(3, "1", 4)])
+    expected.add_transitions([(4, "1", 0)])
+
+    gw = GraphWorker(
+        create_labeled_two_cycles_graph(
+            2,
+            "0",
+            2,
+            "1",
+        )
+    )
 
     assert expected == gw.convert_to_nfa(final={0})
--- tests/regex_dfa_nfa/test_regex_dfa.py
+++ tests/regex_dfa_nfa/test_regex_dfa.py
@@ -8,6 +8,20 @@ def test_regex_to_minimal_dfa():
     assert DeterministicFiniteAutomaton() == regex_to_minimal_dfa(Regex(""))
 
 
+def test_regex_to_minimal_dfa_epsilon():
+    expected = DeterministicFiniteAutomaton()
+    expected.add_start_state(0)
+    expected.add_final_state(0)
+
+    actual = regex_to_minimal_dfa(Regex("$"))
+    print(actual.to_dict())
+
+    assert expected == actual
+    assert actual.is_deterministic() and len(actual.states) == len(
+        actual.minimize().states
+    )
+
+
 def test_regex_to_minimal_dfa_star():
     expected = DeterministicFiniteAutomaton()
     expected.add_start_state(0)
@@ -22,7 +36,7 @@ def test_regex_to_minimal_dfa_star():
     )
 
 
-def test_convert_regex_to_minimal_dfa_or_plus():
+def test_regex_to_minimal_dfa_or_plus():
     expected = DeterministicFiniteAutomaton()
     expected.add_start_state(0)
     expected.add_final_state(1)
@@ -48,6 +62,51 @@ def test_regex_to_minimal_space():
     expected.add_transitions([(0, "test_one", 1), (1, "test_two", 2)])
 
     actual = regex_to_minimal_dfa(Regex("test_one test_two"))
+
+    assert expected == actual
+    assert actual.is_deterministic() and len(actual.states) == len(
+        actual.minimize().states
+    )
+
+
+def test_regex_to_minimal_dfa_var():
+    expected = DeterministicFiniteAutomaton()
+    expected.add_start_state(0)
+    expected.add_final_state(1)
+    expected.add_transitions([(0, "test", 1)])
+
+    actual = regex_to_minimal_dfa(Regex("test"))
+    print(actual.to_dict())
+
+    assert expected == actual
+    assert actual.is_deterministic() and len(actual.states) == len(
+        actual.minimize().states
+    )
+
+
+def test_regex_to_minimal_dfa_parens():
+    expected = DeterministicFiniteAutomaton()
+    expected.add_start_state(0)
+    expected.add_final_state(1)
+    expected.add_transitions([(0, "test", 1)])
+
+    actual = regex_to_minimal_dfa(Regex("(test)"))
+
+    assert expected == actual
+    assert actual.is_deterministic() and len(actual.states) == len(
+        actual.minimize().states
+    )
+
+
+def test_regex_to_minimal_dfa_hard_expr():
+    expected = DeterministicFiniteAutomaton()
+    expected.add_start_state(0)
+    expected.add_transitions([(0, "0", 0)])
+    expected.add_transitions([(0, "1", 0)])
+    expected.add_transitions([(0, "111", 1)])
+    expected.add_final_state(1)
+
+    actual = regex_to_minimal_dfa(Regex("(0|1)*111"))
 
     assert expected == actual
     assert actual.is_deterministic() and len(actual.states) == len(
