68bec693bd12cff5be3881dfcfe19ebc68d57078
AlexanderZadorozhnyy <alexander.zadorozhnyy@yandex.ru>
2023-09-26 23:39:48 +0300

    Updated tests after renaming regex_to_minimal_dfa

--- tests/regex_dfa_nfa/test_regex_dfa.py
+++ tests/regex_dfa_nfa/test_regex_dfa.py
@@ -3,18 +3,18 @@ from os import path
 from pyformlang.finite_automaton import DeterministicFiniteAutomaton
 from pyformlang.regular_expression import Regex
 
-from project.regex_dfa_builder.min_dfa_builder import regex_to_minimal_dfa
+from project.regex_and_automata.regex_func import convert_regex_to_minimal_dfa
 from tests.utils import check_is_dot_files_the_same
 
 CURR_PATH = path.dirname(path.realpath(__file__))
 
 
 def test_regex_to_minimal_dfa():
-    assert DeterministicFiniteAutomaton() == regex_to_minimal_dfa(Regex(""))
+    assert DeterministicFiniteAutomaton() == convert_regex_to_minimal_dfa(Regex(""))
 
 
 def test_regex_to_minimal_dfa_epsilon():
-    actual = regex_to_minimal_dfa(Regex("$"))
+    actual = convert_regex_to_minimal_dfa(Regex("$"))
     actual.write_as_dot(path.join(CURR_PATH, "actual_dfa_epsilon.dot"))
 
     assert check_is_dot_files_the_same(
@@ -26,7 +26,7 @@ def test_regex_to_minimal_dfa_epsilon():
 
 
 def test_regex_to_minimal_dfa_star():
-    actual = regex_to_minimal_dfa(Regex("test*"))
+    actual = convert_regex_to_minimal_dfa(Regex("test*"))
     actual.write_as_dot(path.join(CURR_PATH, "actual_dfa_star.dot"))
 
     assert check_is_dot_files_the_same(
@@ -38,7 +38,7 @@ def test_regex_to_minimal_dfa_star():
 
 
 def test_regex_to_minimal_dfa_or():
-    actual = regex_to_minimal_dfa(Regex("test_one|test_two"))
+    actual = convert_regex_to_minimal_dfa(Regex("test_one|test_two"))
     actual.write_as_dot(path.join(CURR_PATH, "actual_dfa_or.dot"))
 
     assert check_is_dot_files_the_same(
@@ -52,7 +52,7 @@ def test_regex_to_minimal_dfa_or():
 
 
 def test_regex_to_minimal_space():
-    actual = regex_to_minimal_dfa(Regex("test_one test_two"))
+    actual = convert_regex_to_minimal_dfa(Regex("test_one test_two"))
     actual.write_as_dot(path.join(CURR_PATH, "actual_dfa_space.dot"))
 
     assert check_is_dot_files_the_same(
@@ -64,7 +64,7 @@ def test_regex_to_minimal_space():
 
 
 def test_regex_to_minimal_dfa_var():
-    actual = regex_to_minimal_dfa(Regex("test"))
+    actual = convert_regex_to_minimal_dfa(Regex("test"))
     actual.write_as_dot(path.join(CURR_PATH, "actual_dfa_var.dot"))
 
     assert check_is_dot_files_the_same(
@@ -76,7 +76,7 @@ def test_regex_to_minimal_dfa_var():
 
 
 def test_regex_to_minimal_dfa_parens():
-    actual = regex_to_minimal_dfa(Regex("(test)"))
+    actual = convert_regex_to_minimal_dfa(Regex("(test)"))
     actual.write_as_dot(path.join(CURR_PATH, "actual_dfa_parens.dot"))
 
     assert check_is_dot_files_the_same(
@@ -88,7 +88,7 @@ def test_regex_to_minimal_dfa_parens():
 
 
 def test_regex_to_minimal_dfa_hard_expr_one():
-    actual = regex_to_minimal_dfa(Regex("(0|1)*111"))
+    actual = convert_regex_to_minimal_dfa(Regex("(0|1)*111"))
     actual.write_as_dot(path.join(CURR_PATH, "actual_dfa_hard_expr.dot"))
 
     assert check_is_dot_files_the_same(
@@ -103,7 +103,7 @@ def test_regex_to_minimal_dfa_hard_expr_one():
 
 
 def test_regex_to_minimal_dfa_hard_two_expr():
-    actual = regex_to_minimal_dfa(Regex("(0|1)*1.1.1"))
+    actual = convert_regex_to_minimal_dfa(Regex("(0|1)*1.1.1"))
     actual.write_as_dot(path.join(CURR_PATH, "actual_dfa_hard_two_expr.dot"))
 
     assert check_is_dot_files_the_same(
