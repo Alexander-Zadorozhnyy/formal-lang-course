23addb989b5e6a5172707f45048d8b84113a6d8a
AlexanderZadorozhnyy <alexander.zadorozhnyy@yandex.ru>
2023-11-12 23:39:09 +0300

    Added new test cases for ecfg

--- tests/cfg_ecfg/test_ecfg.py
+++ tests/cfg_ecfg/test_ecfg.py
@@ -5,7 +5,19 @@ from pyformlang.cfg import Variable
 
 from project.cfg.cfg_func import read_context_free_grammar
 from project.ecfg.ecfg import ECFG
+from project.regex_and_automata.regex_func import convert_regex_to_minimal_dfa
 from tests.cfg_ecfg.cases_for_testing_ecfg import ecfg_cases, start_symbol
+
+
+def helper_compare_ecfg_productions(ecfg, exp_ecfg_productions):
+    ecfg_productions = ecfg.productions
+
+    for key, value in ecfg_productions.items():
+        a = convert_regex_to_minimal_dfa(value)
+        b = convert_regex_to_minimal_dfa(exp_ecfg_productions[key])
+        assert a.is_equivalent_to(b)
+
+    assert len(ecfg_productions) == len(exp_ecfg_productions)
 
 
 def test_convert_cfg_to_ecfg():
@@ -29,10 +41,12 @@ def test_convert_srt_to_ecfg():
     for srt_ecfg, exp_variables, exp_productions in ecfg_cases:
         ecfg = ECFG.convert_str_to_ecfg(srt_ecfg, start_symbol)
 
-        assert ecfg.variables == set(map(lambda var: Variable(var), exp_variables))
-        assert ecfg.start_symbol == start_symbol
-
         variables = []
         for var in ecfg.productions.keys():
             assert var not in variables
             variables.append(var)
+
+        assert ecfg.variables == set(map(lambda var: Variable(var), exp_variables))
+        assert ecfg.start_symbol == start_symbol
+
+        helper_compare_ecfg_productions(ecfg, exp_productions)
