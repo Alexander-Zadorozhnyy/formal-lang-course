2ab27645132c7378cfb62665982ad8f7ba1cc8ca
AlexanderZadorozhnyy <alexander.zadorozhnyy@yandex.ru>
2023-11-14 14:35:53 +0300

    Fix bug with paths

--- tests/hellings/test_hellings.py
+++ tests/hellings/test_hellings.py
@@ -12,21 +12,24 @@ from tests.hellings.cases_for_testing import (
 )
 
 
+CURR_PATH = os.path.dirname(os.path.realpath(__file__))
+
+
 def test_hellings_algorithms():
     for (graph, cfg, expected_result) in hellings_algorithms_test_cases:
-        cfg = Path(os.path.join(os.getcwd(), "static", "cfg", cfg))
+        cfg = Path(os.path.join(CURR_PATH, "static", "cfg", cfg))
 
         if isinstance(graph, str):
-            graph = read_dot(Path(os.path.join(os.getcwd(), "static", "graph", graph)))
+            graph = read_dot(Path(os.path.join(CURR_PATH, "static", "graph", graph)))
 
         assert hellings(graph, cfg) == expected_result
 
 
 def test_hellings_cfpq():
     for (graph, cfg, start, final, expected_result) in hellings_cfpq_test_cases:
-        cfg = Path(os.path.join(os.getcwd(), "static", "cfg", cfg))
+        cfg = Path(os.path.join(CURR_PATH, "static", "cfg", cfg))
 
         if isinstance(graph, str):
-            graph = read_dot(Path(os.path.join(os.getcwd(), "static", "graph", graph)))
+            graph = read_dot(Path(os.path.join(CURR_PATH, "static", "graph", graph)))
 
         assert cfpq(graph, cfg, start, final) == expected_result
