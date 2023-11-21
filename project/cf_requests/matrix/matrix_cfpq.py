bf0fc8f9ea6b17852e26d6350d87a1ee74e7f235
AlexanderZadorozhnyy <alexander.zadorozhnyy@yandex.ru>
2023-11-21 14:45:49 +0300

    Implemented cfpq based on matrix multiplication

---
+++ project/cf_requests/matrix/matrix_cfpq.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from typing import Union
+
+import networkx as nx
+from pyformlang.cfg import CFG
+
+from project.cf_requests.matrix.matrix_alg import matrix_transitive_closure
+
+
+def cfpq(
+        graph: nx.MultiDiGraph,
+        request: Union[Path, CFG],
+        start_nodes: set = None,
+        final_nodes: set = None,
+        start_variable: str = "S",
+) -> dict:
+    if not start_nodes:
+        start_nodes = set(map(int, filter(lambda x: x != "\\n", graph.nodes)))
+
+    if not final_nodes:
+        final_nodes = set(map(int, filter(lambda x: x != "\\n", graph.nodes)))
+
+    return {
+        sn: set(
+            final
+            for start, var, final in list(
+                filter(lambda x: x[0] == sn, matrix_transitive_closure(graph, request, start_variable))
+            )
+            if final in final_nodes
+        )
+        for sn in start_nodes
+    }
