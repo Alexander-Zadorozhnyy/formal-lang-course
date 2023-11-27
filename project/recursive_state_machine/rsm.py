c702f5e08f393ecd52860afe81c954b6b9dd1e7a
AlexanderZadorozhnyy <alexander.zadorozhnyy@yandex.ru>
2023-11-27 11:15:51 +0300

    Add possibility to get_rsm_binary_matrix

--- project/recursive_state_machine/rsm.py
+++ project/recursive_state_machine/rsm.py
@@ -1,10 +1,13 @@
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Union
+from typing import Union, Type
 
 from pyformlang.cfg import Variable
+from pyformlang.finite_automaton import State
+from scipy.sparse import dok_matrix, lil_matrix, csr_matrix, csc_matrix
 
 from project.ecfg.ecfg import ECFG
+from project.matrix.matrix import Matrix
 
 
 @dataclass
@@ -34,4 +37,45 @@ class RSM:
             automatas={
                 key: automata.minimize() for key, automata in self.automatas.items()
             },
+        )
+
+    def get_binary_matrix(
+        self,
+        typed_matrix: Union[
+            Type[lil_matrix], Type[dok_matrix], Type[csr_matrix], Type[csc_matrix]
+        ],
+    ) -> Matrix:
+        states, start_states, final_states = set(), set(), set()
+        for v, dfa in self.automatas.items():
+            for s in dfa.states:
+                state = State((v, s.value))
+                states.add(state)
+                if s in dfa.start_states:
+                    start_states.add(state)
+                if s in dfa.final_states:
+                    final_states.add(state)
+        states = list(states)
+        states.sort(key=lambda state: (state.value[0].value, state.value[1]))
+
+        indexes = {state: index for index, state in enumerate(states)}
+        labels = dict()
+
+        for v, dfa in self.automatas.items():
+            for state_from, transitions in dfa.to_dict().items():
+                for label, states_to in transitions.items():
+                    states_to = states_to if isinstance(states_to, set) else {states_to}
+                    for state_to in states_to:
+                        if label not in labels:
+                            labels[label] = typed_matrix(
+                                (len(states), len(states)), dtype=bool
+                            )
+
+                        i = indexes[State((v, state_from.value))]
+                        j = indexes[State((v, state_to.value))]
+                        labels[label][i, j] = True
+        return Matrix(
+            start_states=start_states,
+            final_states=final_states,
+            indexes=indexes,
+            labels=labels,
         )
--- tests/test_rsm/test_rsm.py
+++ tests/test_rsm/test_rsm.py
@@ -1,6 +1,9 @@
+from scipy.sparse import dok_matrix
+
 from project.ecfg.ecfg import ECFG
 from project.recursive_state_machine.rsm import RSM
 from tests.cfg_ecfg.cases_for_testing_ecfg import ecfg_cases, start_symbol
+from tests.test_rsm.cases_for_testing import rsm_binary_matrix_cases
 
 
 def test_convert_ecfg_to_rsm():
@@ -21,3 +24,27 @@ def test_minimize_rsm():
 
         for var in rsm.automatas:
             assert rsm_minimized.automatas[var].is_equivalent_to(rsm.automatas[var])
+
+
+def test_get_rsm_binary_matrix():
+    for srt_ecfg, expected_states, expected_matrix in rsm_binary_matrix_cases:
+        ecfg = ECFG.convert_str_to_ecfg(srt_ecfg, start_symbol)
+        rsm = RSM.convert_ecfg_to_rsm(ecfg, start_symbol)
+
+        binary_matrix = rsm.get_binary_matrix(dok_matrix)
+
+        assert len(binary_matrix.indexes) == len(expected_states)
+        for index, state in enumerate(binary_matrix.indexes):
+            expected = expected_states[index]
+            assert (state.value[0].value, state.value[1]) == expected[0]
+            assert (state in binary_matrix.start_states) == expected[1]
+            assert (state in binary_matrix.final_states) == expected[2]
+
+        assert binary_matrix.get_number_of_labels() == len(expected_matrix.keys())
+        for key in binary_matrix.labels.keys():
+            result = []
+            matrix = binary_matrix.labels[key].nonzero()
+            for index in range(len(matrix[0])):
+                result.append([matrix[0][index], matrix[1][index]])
+
+            assert result == expected_matrix[str(key)]
